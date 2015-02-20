21d18292378c715b61c44a42dcc1e73652b4c591
Jan Zak <zj@zakjan.cz>
2015-02-20 16:16:55 +0800

    added cert with PEM intermediate to tests

--- tests/run.sh
+++ tests/run.sh
@@ -7,12 +7,18 @@ set -u # fail on undefined variable
 TESTS_DIR="$(dirname $0)"
 CMD="$TESTS_DIR/../resolve.sh"
 
-# it should download all intermediate CA certs for PEM input
-$CMD "$TESTS_DIR/input.crt" "$TESTS_DIR/output.crt"
-diff "$TESTS_DIR/output.crt" "$TESTS_DIR/output.expected.crt"
+
+# it should download all intermediate CA certs - Comodo, PEM leaf, 2x DER intermediate
+$CMD "$TESTS_DIR/comodo.crt" "$TESTS_DIR/output.crt"
+diff "$TESTS_DIR/output.crt" "$TESTS_DIR/comodo.bundle.crt"
 rm "$TESTS_DIR/output.crt"
 
-# it should download all intermediate CA certs for DER input
-$CMD "$TESTS_DIR/input.der.crt" "$TESTS_DIR/output.crt"
-diff "$TESTS_DIR/output.crt" "$TESTS_DIR/output.expected.crt"
+# it should download all intermediate CA certs - Comodo, DER leaf, 2x DER intermediate
+$CMD "$TESTS_DIR/comodo.der.crt" "$TESTS_DIR/output.crt"
+diff "$TESTS_DIR/output.crt" "$TESTS_DIR/comodo.bundle.crt"
+rm "$TESTS_DIR/output.crt"
+
+# it should download all intermediate CA certs - GoDaddy, PEM leaf, PEM intermediate
+$CMD "$TESTS_DIR/godaddy.crt" "$TESTS_DIR/output.crt"
+diff "$TESTS_DIR/output.crt" "$TESTS_DIR/godaddy.bundle.crt"
 rm "$TESTS_DIR/output.crt"
