9a14257c5e774238a6904cf1eaf95d1502e496d0
Jan Zak <zj@zakjan.cz>
2015-11-10 19:45:38 +0100

    Add tests for certificates with multiple issuer URLs

--- tests/run.sh
+++ tests/run.sh
@@ -11,17 +11,21 @@ TEMP_FILE="$(mktemp)"
 (
     set -e
 
-    # it should output certificate bundle in PEM format with Comodo, PEM leaf, 2x DER intermediate
+    # it should output certificate bundle in PEM format with certificate from Comodo, PEM leaf, 2x DER intermediate
     $CMD -o "$TEMP_FILE" "$DIR/comodo.crt"
     diff "$TEMP_FILE" "$DIR/comodo.bundle.crt"
 
-    # it should output certificate bundle in PEM format with Comodo, DER leaf, 2x DER intermediate
+    # it should output certificate bundle in PEM format with certificate from Comodo, DER leaf, 2x DER intermediate
     $CMD -o "$TEMP_FILE" "$DIR/comodo.der.crt"
     diff "$TEMP_FILE" "$DIR/comodo.bundle.crt"
 
-    # it should output certificate bundle in PEM format with GoDaddy, PEM leaf, PEM intermediate
+    # it should output certificate bundle in PEM format with certificate from GoDaddy, PEM leaf, PEM intermediate
     $CMD -o "$TEMP_FILE" "$DIR/godaddy.crt"
     diff "$TEMP_FILE" "$DIR/godaddy.bundle.crt"
+
+    # it should output certificate bundle in PEM format with certificate with multiple issuer URLs
+    $CMD -o "$TEMP_FILE" "$DIR/multiple-issuer-urls.crt"
+    diff "$TEMP_FILE" "$DIR/multiple-issuer-urls.bundle.crt"
 
     # it should output certificate bundle in DER format
     $CMD -d -o "$TEMP_FILE" "$DIR/comodo.crt"
