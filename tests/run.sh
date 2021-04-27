61debd8e0ad0602c9e57b4281be9d0377b20b859
Jan Zak <zj@zakjan.cz>
2021-04-27 07:12:41 +0200

    Update tests

--- tests/run.sh
+++ tests/run.sh
@@ -11,68 +11,67 @@ TEMP_FILE="$(mktemp)"
 (
     set -e
 
-    # it should output certificate bundle in PEM format with certificate from Comodo, PEM leaf, 2x DER intermediate
+    echo 'it should output certificate bundle in PEM format (Comodo, PEM leaf, 2x DER intermediate)'
     $CMD -o "$TEMP_FILE" "$DIR/comodo.crt"
     diff "$TEMP_FILE" "$DIR/comodo.bundle.crt"
 
-    # it should output certificate bundle in PEM format with certificate from Comodo, DER leaf, 2x DER intermediate
+    echo 'it should output certificate bundle in PEM format (Comodo, DER leaf, 2x DER intermediate)'
     $CMD -o "$TEMP_FILE" "$DIR/comodo.der.crt"
     diff "$TEMP_FILE" "$DIR/comodo.bundle.crt"
 
-    # it should output certificate bundle in PEM format with certificate from GoDaddy, PEM leaf, PEM intermediate
+    echo 'it should output certificate bundle in PEM format (GoDaddy, PEM leaf, PEM intermediate)'
     $CMD -o "$TEMP_FILE" "$DIR/godaddy.crt"
     diff "$TEMP_FILE" "$DIR/godaddy.bundle.crt"
 
-    # it should output certificate bundle in PEM format with certificate with multiple issuer URLs
-    $CMD -o "$TEMP_FILE" "$DIR/multiple-issuer-urls.crt"
-    diff "$TEMP_FILE" "$DIR/multiple-issuer-urls.bundle.crt"
+    echo 'it should output certificate bundle in PEM format (zedat.fu-berlin.de, multiple issuer URLs)'
+    $CMD -o "$TEMP_FILE" "$DIR/zedat.crt"
+    diff "$TEMP_FILE" "$DIR/zedat.bundle.crt"
 
-    # it should output certificate bundle in DER format
+    echo 'it should output certificate bundle in DER format (Comodo, PEM leaf, 2x DER intermediate)'
     $CMD -d -o "$TEMP_FILE" "$DIR/comodo.crt"
     diff "$TEMP_FILE" "$DIR/comodo.bundle.der.crt"
 
-    # it should output certificate chain in PEM format
+    echo 'it should output certificate chain in PEM format (Comodo, PEM leaf, 2x DER intermediate)'
     $CMD -i -o "$TEMP_FILE" "$DIR/comodo.crt"
     diff "$TEMP_FILE" "$DIR/comodo.chain.crt"
 
-    # it should output certificate chain in DER format
+    echo 'it should output certificate chain in DER format (Comodo, PEM leaf, 2x DER intermediate)'
     $CMD -d -i -o "$TEMP_FILE" "$DIR/comodo.crt"
     diff "$TEMP_FILE" "$DIR/comodo.chain.der.crt"
 
-    # it should output certificate bundle in PEM format, with input from stdin and output to stdout
+    echo 'it should output certificate bundle in PEM format with root CA from system (Comodo, PEM leaf, 2x DER intermediate)'
+    $CMD -s -o "$TEMP_FILE" "$DIR/comodo.crt"
+    diff "$TEMP_FILE" "$DIR/comodo.withca.crt"
+
+    echo 'it should output certificate bundle in PEM format with root CA from system (zedat.fu-berlin.de, multiple issuer URLs)'
+    $CMD -s -o "$TEMP_FILE" "$DIR/zedat.crt"
+    diff "$TEMP_FILE" "$DIR/zedat.withca.bundle.crt"
+
+    echo 'it should output certificate bundle in PEM format (DST Root CA X3, PKCS#7 leaf)'
+    $CMD -o "$TEMP_FILE" "$DIR/dstrootcax3.p7c"
+    diff "$TEMP_FILE" "$DIR/dstrootcax3.pem"
+
+    echo 'it should output certificate bundle in PEM format with input from stdin and output to stdout (Comodo, PEM leaf, 2x DER intermediate)'
     $CMD < "$DIR/comodo.crt" > "$TEMP_FILE"
     diff "$TEMP_FILE" "$DIR/comodo.bundle.crt"
 
-    # Append CA root cert to output
-    $CMD -s < "$DIR/comodo.crt" > "$TEMP_FILE"
-    diff "$TEMP_FILE" "$DIR/comodo.withca.crt"
-
-    # Already has CA root cert
-    $CMD -s < "$DIR/multiple-issuer-urls.crt" > "$TEMP_FILE"
-    diff "$TEMP_FILE" "$DIR/multiple-issuer-urls.withca.bundle.crt"
-
-    # DST Root CA X3, PKCS#7 package
-    $CMD < "$DIR/dstrootcax3.p7c" > "$TEMP_FILE"
-    diff "$TEMP_FILE" "$DIR/dstrootcax3.pem"
-
-    # it should detect invalid certificate
+    echo 'it should detect invalid certificate'
     (
-         set +e
-         ! echo "xxx" | $CMD
+        set +e
+        ! echo "xxx" | $CMD
     )
 
-    # Build and start the webserver to serve the certificates
+    # build and start the webserver to serve the certificates
     go build -o  "${DIR}/http-server" "${DIR}/http-server.go"
     "${DIR}/http-server" "${DIR}" &
     PID="$!"
     sleep 1
 
-    # It should correctly detect root certificates to prevent infinite traversal loops when the root
-    # certificate also has an AIA Certification Authority Issuer record
-    $CMD < "$DIR/self-issued.crt" > "$TEMP_FILE"
+    echo 'it should correctly detect root certificates to prevent infinite traversal loops when the root certificate also has an AIA Certification Authority Issuer record'
+    $CMD -o "$TEMP_FILE" "$DIR/self-issued.crt"
     diff "$TEMP_FILE" "$DIR/self-issued.bundle.crt"
 
-    # Stop the webserver
+    # stop the webserver
     kill "$PID"
 )
 STATUS="$?"
